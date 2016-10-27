e9895017a4cf62d652551e86660e964a875209da
Rodwyn <rodwynmoreno@hotmail.com>
2016-10-27 00:22:18 -0500

    Refactored gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,106 @@ var	gulp =	require('gulp'),
 		useref = require('gulp-useref'),
     templateCache	=	require('gulp-angular-templatecache'),
 		sass = require('gulp-sass'),
-		minifyCss	=	require('gulp-minify-css');
-    //wiredep	=	require('wiredep').stream;
+		sassLint = require('gulp-sass-lint'),
+		minifyCss	=	require('gulp-minify-css'),
+    wiredep	=	require('wiredep').stream;
 
-    gulp.task('server',	function() {
-    	connect.server({
-    		root:	'./app',
-    		hostname:	'0.0.0.0',
-    		port:	8080,
-    		livereload:	true
-    	});
-    });
 
-    gulp.task('livereload',	function() {
-    	gulp.src('./app/**/*.html')
-    		.pipe(connect.reload());
-    });
+gulp.task('server',	function() {
+	connect.server({
+		root:	'./app',
+		hostname:	'0.0.0.0',
+		port:	8080,
+		livereload:	true
+	});
+});
 
-    gulp.task('styles', function() {
+gulp.task('livereload',	function() {
+	gulp.src('./app/**/*.html')
+		.pipe(connect.reload());
+});
+
+gulp.task('scsslint', function() {
+	return gulp.src('./app/stylesheets/**/*.s+(a|c)ss')
+		.pipe(sassLint({
+			config: '.sass-lint.yml',
+			files: {
+				ignore: './app/stylesheets/reset.scss'
+			},
+		}))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+
+gulp.task('styles', function() {
   return gulp.src('./app/stylesheets/main.scss')
     .pipe(sass({
       outputStyle: "compressed"
     }).on('error', sass.logError))
     .pipe(gulp.dest('./app/stylesheets'))
 		.pipe(connect.reload());
-  });
+});
 
-  gulp.task('inject', function() {
-	var sources = gulp.src(['./app/stylesheets/**/*.css'], {read:false});
+gulp.task('jshint',	function() {
+	return gulp.src('./app/scripts/**/*.js')
+  	.pipe(jscs({configPath: '.jscsrc'}))
+  	.pipe(jscs.reporter());
+});
+
+gulp.task('inject', function() {
+	var sources = gulp.src(['./app/stylesheets/**/*.css', './app/scripts/**/*.js'], {read:false});
 	return gulp.src('./app/index.html')
 		.pipe(inject(sources, {relative: true}))
 		.pipe(gulp.dest('./app'));
-  });
+});
 
-  /*gulp.task('inject-bower-components', function () {
+gulp.task('inject-bower-components', function () {
 	gulp.src('./app/index.html')
 		.pipe(wiredep({
 			directory:	'./app/lib'
 		}))
 		.pipe(gulp.dest('./app'));
-  });*/
-
-  gulp.task('watch', function() {
-  gulp.watch(['./app/**/*.html'], ['livereload']);
-	gulp.watch(['./app/stylesheets/**/*.scss'],	['styles', 'inject']);
-  //gulp.watch(['./bower.json'], ['inject-bower-components']);
 });
 
-gulp.task('default', ['styles', 'watch', 'inject', 'server']);
+gulp.task('templates',	function()	{
+	gulp.src('./app/views/**/*.tpl.html')
+		.pipe(templateCache({
+			root:	'views/',
+			module:	'frontEndApp.templates',
+			standalone:	true
+		}))
+		.pipe(gulp.dest('./app/scripts'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./app/**/*.html'], ['livereload', 'templates']);
+	gulp.watch(['./app/stylesheets/**/*.scss'],	['scsslint', 'styles']);
+  gulp.watch(['./app/scripts/**/*.js', './gulpfile.js'], ['jshint']);
+});
+
+gulp.task('default', ['templates', 'styles', 'server', 'watch']);
+
+
+gulp.task('compress',	function()	{
+	gulp.src('./app/index.html')
+		.pipe(useref())
+		.pipe(gulpif('*.css',	minifyCss()))
+		.pipe(gulpif('*.js', uglify({ mangle: false })))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy',	function()	{
+	gulp.src('./app/index.html')
+		.pipe(useref())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('dist-server',	function()	{
+	connect.server({
+		root:	'./dist',
+		hostname:	'0.0.0.0',
+		port:	8088,
+	});
+});
+
+gulp.task('build', ['templates', 'compress', 'copy', 'dist-server']);
